ae6a0cf54c5afc425b36530262f62114b5c95b8a
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2026-06-20 20:39:24 +0530

    Add amount input handling and incrementByAmount functionality to Counter component

--- redux/src/components/Counter.jsx
+++ redux/src/components/Counter.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment,reset } from '../features/counter/counterSlice'
+import { decrement, increment,reset,incrementByAmount } from '../features/counter/counterSlice'
 
 
 const Counter = () => {
+    const[amount, setAmount] = useState(0)
     const count = useSelector((state) => state.counter.value)
     const dispatch = useDispatch()
     
@@ -22,12 +23,19 @@ const Counter = () => {
     const handleReset = () => {
         dispatch(reset())
     }
+    const handleAmount = () => {
+        const value = amount
+        dispatch(incrementByAmount(value))
+        value = ''     
+    }
   return (
     <div>
       <h1>Counter {count}</h1>
       <button onClick={handleIncrement}>Increment</button>
       <button onClick={handleDecrement}>Decrement</button>
       <button onClick={handleReset}>Reset</button>
+      <input onChange={(e) => setAmount(e.target.value)} type="text" placeholder='Enter Amount' />
+      <button onClick={handleAmount}>Send</button>
       
     </div>
   )
